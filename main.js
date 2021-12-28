9ab078bf325844d22ff9121cf5889a7c5b71a6f8
Santhanu236 <santhanu236@gmail.com>
2021-12-28 12:17:25 +0530

    'fixed'

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function timelyMsg(){
     const date = new Date();
     const hours = date.getHours();
-    if (hours > 5 && hours < 12){
+    if (hours >= 5 && hours < 12){
         console.log('morning');
         document.getElementById('imgdiv').innerHTML = '<img src="./assets/img/sunrise.png" style="height: 50px;">';
         document.getElementById('msg').innerHTML = '<h2>Good Morning!</h2>';
     }
-    else if (hours > 12 && hours < 17){
+    else if (hours >= 12 && hours < 17){
         console.log('afternoon');
         document.getElementById('imgdiv').innerHTML = '<img src="./assets/img/mid-day.png" style="height: 50px;">';
         document.getElementById('msg').innerHTML = '<h2>Good Afternoon!</h2>';
     }
-    else if(hours > 17 && hours < 20){
+    else if(hours >= 17 && hours < 20){
         console.log('evening');
         document.getElementById('imgdiv').innerHTML = '<img src="./assets/img/sunset.png" style="height: 50px;">';
         document.getElementById('msg').innerHTML = '<h2>Good Evening!</h2>';
